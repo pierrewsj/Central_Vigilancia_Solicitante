3d364866b73ff2ef21233d5ac42aba9ca50f5c1c
pierrewsj <pierrewenderson1@gmail.com>
2026-09-02 19:47:39 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.GSP_CONFIG = {
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyGXzY4QQneIejH18IWyq2El_vdloQtMxt7rGZ7nCViyqpGwiATvnD7x4QBAADSNexN/exec',
+  APP_VERSION: 'GSP-V12-CONEXAO-NOVA'
+};
