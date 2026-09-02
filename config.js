9a038dd3d6e0449b5e6092dff714968e626602cd
pierrewsj <pierrewenderson1@gmail.com>
2026-09-02 20:27:25 -0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-window.GSP_CONFIG = {
-  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyGXzY4QQneIejH18IWyq2El_vdloQtMxt7rGZ7nCViyqpGwiATvnD7x4QBAADSNexN/exec',
-  APP_VERSION: 'GSP-V13-AJUSTES-PARTE-1'
-};
